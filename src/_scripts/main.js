600f0068b2a272c08e215df302b77ace7752dc54
dudeletslive <dannycansignup@gmail.com>
2016-05-06 14:18:11 -0600

    init

---
+++ src/_scripts/main.js
@@ -0,0 +1,22 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+var $ = require('jquery');
+
+/*
+$(function() {
+  var $form    = $(document.getElementById("register")),
+      form_email = $(document.getElementById("form-EMAIL")),
+      MCE_email = $(document.getElementById("mce-EMAIL")),
+      MCE_form = $(document.getElementById("mc-embedded-subscribe-form"));
+
+  $form.on("submit", function() {
+    MCE_email.val($form_email.val());
+    MCE_form.submit()
+    return false;
+  });
+});
+
+*/
